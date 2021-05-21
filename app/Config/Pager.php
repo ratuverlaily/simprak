5d013d66e2403fa848b995777d2afe26fbf2a8c9
ratuverlaily <lugiverlaily@gmail.com>
2021-05-22 06:45:33 +0700

    Update Pager.php

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,6 +24,9 @@ class Pager extends BaseConfig
 		'default_full'   => 'CodeIgniter\Pager\Views\default_full',
 		'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
 		'default_head'   => 'CodeIgniter\Pager\Views\default_head',
+		'posting_pagination' => 'App\Views\Pagers\posting_pagination',
+		'praktikum_pagination' => 'App\Views\Pagers\praktikum_pagination',
+		'praktikumnilai_pagination' => 'App\Views\Pagers\praktikumnilai_pagination',
 	];
 
 	/**
