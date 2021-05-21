5aa196880c5a155809db4b2ea14c21c8e43628ed
ratuverlaily <lugiverlaily@gmail.com>
2021-05-22 06:46:50 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -57,29 +57,53 @@ $routes->group('users', function ($routes) {
 	$routes->get('guru', 'Users::aktifasiKelas');
 	$routes->post('aktivasi/kelas', 'Users::addaktivasikelas');
 	$routes->get('sekolah', 'Users::viewsekolah');
+	$routes->post('posting', 'Users::postPosting');
+	$routes->get('posting/hapus/(:any)', 'Users::hapusPosting/$1');
+	$routes->post('komentar/(:num)', 'Users::postKomentar/$1');
+	$routes->get('komentar/view/(:num)', 'Users::viewkomentar/$1');
+	$routes->get('komentar/detail/(:num)', 'Users::detailkomentar/$1');
+	$routes->get('komentar/orang/(:any)', 'Users::hapusKomentarOrang/$1');
+	$routes->get('komentar/hapus/(:num)', 'Users::hapuskomentar/$1');
+	$routes->get('komentar/pdf/(:num)', 'Users::viewkomentarpdf/$1');
 });
 
 $routes->group('praktikum', function ($routes) {
 	$routes->get('nilai', 'Praktikum::viewNilaiSiswaPraktikum');
 	$routes->get('siswa', 'Praktikum::getpraktikumkelas');
 	$routes->get('detail/(:num)', 'Praktikum::praktikumdetail/$1');
+	$routes->get('modul/unduh/(:num)', 'Praktikum::viewpdfpraktikum/$1');
 	$routes->post('mulai', 'Praktikum::mulaipraktikum');
 	$routes->get('guru/nilai', 'Praktikum::viewNilaiGuruPraktikum');
 	$routes->get('penilaian/detail/(:num)', 'Praktikum::viewNilaiGuruPraktikumDetail/$1');
 	$routes->get('guru/list', 'Praktikum::viewPraktikumList');
+	$routes->get('guru/detail/(:num)', 'Praktikum::praktikumgurudetail/$1');
+	$routes->get('guru/ubahview/(:num)', 'Praktikum::viewUpdatePraktikum/$1');
 	$routes->post('tambah', 'Praktikum::praktikum_add');
 	$routes->post('ubah', 'Praktikum::praktikum_update');
+	$routes->get('hapus/(:num)', 'Praktikum::praktikum_delete/$1');
 	$routes->get('sentkode/(:any)', 'Praktikum::praktikum_getkode/$1');
 });
 
 $routes->group('modul', function ($routes) {
 	$routes->get('siswa', 'Modul::getmodulpraktikumsiswa');
 	$routes->get('guru', 'Modul::getmodulpraktikumguru');
+	$routes->get('file/guru', 'Modul::getfilemodulguru');
+	$routes->get('myfile/guru', 'Modul::getmyfileguru');
+	$routes->post('myfile/simpan', 'Modul::simpanFileGuru');
+	$routes->get('file/siswa', 'Modul::getfilemodulguru');
+	$routes->get('myfile/siswa', 'Modul::getmyfileguru');
+	$routes->get('praktikum/unduh/(:any)', 'Modul::viewModulpdf/$1');
+	$routes->get('file/unduh/(:any)', 'Modul::viewfilepdf/$1');
+	$routes->get('myfile/hapus/(:any)', 'Modul::hapusfilepdf/$1');
+	$routes->get('myfile/unduh/(:any)', 'Modul::viewmyfilepdf/$1');
 });
+
+/* dashboard */
+$routes->get('dashboard', 'Dashboard::index');
 
 
 $routes->get('kelas', 'Kelas::index');
-$routes->get('kelas/siswa', 'Kelas::showKelasSiswa');
+$routes->get('murid', 'Kelas::showKelasSiswa');
 $routes->get('sekolah', 'Sekolah::index');
 /* aktifasi untuk guru */
 $routes->get('kelas/aktivasi/(:num)', 'Kelas::aktivasikelas/$1');
@@ -98,7 +122,11 @@ $routes->group('identitas', function ($routes) {
 	$routes->get('sekolah/edit', 'Users::editViewSekolahguru');
 	$routes->post('sekolah/add', 'Users::simpanSekolahguru');
 	$routes->post('sekolah/update', 'Users::updateViewSekolahguru');
+	$routes->get('password', 'Identitas::viewPassword');
+	$routes->post('password/update', 'Identitas::updatePassword');
 });
+
+
 
 
 
