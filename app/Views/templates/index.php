f955cbee577904fb2123e5c231692e95eb0e6325
ratuverlaily <lugiverlaily@gmail.com>
2021-05-22 12:00:54 +0700

    Update index.php

--- app/Views/templates/index.php
+++ app/Views/templates/index.php
@@ -12,7 +12,7 @@
     <title>My User Siswa</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url() ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url() ?>/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
@@ -54,7 +54,8 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; E-Learning <?= date('Y'); ?></span>
+                        <span>Copyright &copy; E-SIMPRAG <?= date('Y'); ?></span><br /><br />
+                        <span><small> BY RATU VERLAILI E (23219016)</small></span><br />
                     </div>
                 </div>
             </footer>
@@ -93,10 +94,10 @@
     <!-- Bootstrap core JavaScript-->
     <!--<script src="<?= base_url() ?>/vendor/jquery/jquery.min.js"></script>-->
 
-    <script src="<?= base_url() ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_url() ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= base_url() ?>/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="<?= base_url() ?>/js/sb-admin-2.min.js"></script>
