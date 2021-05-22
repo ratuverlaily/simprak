2a641534b45cd6eaf31f98d4d26b626f713f78d7
ratuverlaily <lugiverlaily@gmail.com>
2021-05-22 11:58:45 +0700

    Update Gmodul.php

--- app/Views/t_guru/Gmodul.php
+++ app/Views/t_guru/Gmodul.php
@@ -5,66 +5,66 @@
 <script src="<?= base_url() ?>/js/jquery.dataTables.min.js"></script>
 
 <div class="container-fluid">
-    <div class="col-sm-12">
-        <div class="card m-b-30">
-            <div class="card-body">
+    <div class="alert alert-primary o-hidden shadow-lg" role="alert">
+        <h6 align="center"><b>DAFTAR MODUL</b></h6>
+    </div>
 
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="<?php echo base_url('modul/siswa'); ?>">Modul Praktikum</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Files</a>
-                    </li>
-                </ul>
+    <div class="card m-b-30 o-hidden shadow-lg my-1">
+        <div class="card-body">
 
-                <br />
-                <div class="alert alert-primary" role="alert">
-                    <h6 align="center"><b>DAFTAR MODUL</b></h6>
-                </div>
-                <br />
-                <table id="table_id" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                    <thead>
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" href="<?php echo base_url('modul/guru'); ?>">Modul Praktikum</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url('modul/file/guru'); ?>">Files</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url('modul/myfile/guru'); ?>">Files Saya</a>
+                </li>
+            </ul>
+            <br /><br />
+            <table id="table_id" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Judul</th>
+                        <th>Keterangan</th>
+                        <th>Format</th>
+                        <th>Tanggal</th>
+                        <th style="width:100px;">Action
+                            </p>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $no = 1;
+                    foreach ($moduls as $modul) { ?>
                         <tr>
-                            <th>No</th>
-                            <th>Judul</th>
-                            <th>Keterangan</th>
-                            <th>Format</th>
-                            <th>Tanggal</th>
-                            <th style="width:100px;">Action
-                                </p>
-                            </th>
+                            <td><?php echo $no++; ?></td>
+                            <td><?php echo $modul->judul; ?></td>
+                            <td><?php echo $modul->keterangan; ?></td>
+                            <td><?php echo $modul->format; ?></td>
+                            <td><?php echo $modul->tanggal; ?></td>
+                            <td align="center">
+                                <a class="btn btn-primary" href="<?= base_url(); ?>/modul/praktikum/unduh/<?php echo $modul->id_modul; ?>"><i class="fas fa-download"></i></a>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <?php $no = 1;
-                        foreach ($moduls as $modul) { ?>
-                            <tr>
-                                <td><?php echo $no++; ?></td>
-                                <td><?php echo $modul->judul; ?></td>
-                                <td><?php echo $modul->keterangan; ?></td>
-                                <td><?php echo $modul->format; ?></td>
-                                <td><?php echo $modul->tanggal; ?></td>
-                                <td align="center">
-                                    <a class="btn btn-primary" href="<?= base_url(); ?>/berkas/download/"><i class="fas fa-download"></i></a>
-                                </td>
-                            </tr>
-                        <?php } ?>
+                    <?php } ?>
 
-                    </tbody>
+                </tbody>
 
-                    <tfoot>
-                        <tr>
-                            <th>No</th>
-                            <th>Judul</th>
-                            <th>Keterangan</th>
-                            <th>Format</th>
-                            <th>Tanggal</th>
-                            <th>Action</th>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
+                <tfoot>
+                    <tr>
+                        <th>No</th>
+                        <th>Judul</th>
+                        <th>Keterangan</th>
+                        <th>Format</th>
+                        <th>Tanggal</th>
+                        <th>Action</th>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     </div>
 </div>
