8bf6fb27c02cd8f0a90813551dc9488765b4e948
ratuverlaily <lugiverlaily@gmail.com>
2021-05-25 20:07:35 +0700

    Update Gpraktikumdetail.php

--- app/Views/t_guru/Gpraktikumdetail.php
+++ app/Views/t_guru/Gpraktikumdetail.php
@@ -8,15 +8,28 @@
 <div class="container-fluid">
     <div class="card o-hidden shadow-lg my-5">
         <div class="alert-primary p-3 text-center">
-            <b>PENILAIAN PRAKTIKUM SISWA</b>
+            <b><?php echo $praktikum->judul; ?></b>
         </div>
         <div class="card-body">
-            <div class="bd-highlight"><button type="button" class="btn btn-outline-info" onclick="location.href='<?= base_url('praktikum/guru/nilai'); ?>'"><i class="fas fa-backward"></i> Kembali</button></div>
+            <div class="d-flex">
+                <div class="mr-auto p-1">
+                    <button type="button" class="btn btn-outline-info" onclick="location.href='<?= base_url('praktikum/guru/nilai'); ?>'"><i class="fas fa-backward"></i> Kembali</button>
+                </div>
+                <div class="ml-auto p-2 text-right">
+                    <?php
+                    $date_posting = date_create($praktikum->tgl_publis . " " . $praktikum->waktu_publis);
+                    $date_batas = date_create($praktikum->tgl_batas . " " . $praktikum->waktu_publis);
+                    ?>
+                    Tanggal Posting : <?= date_format($date_posting, "d-m-Y H:i:s"); ?><br />
+                    Tanggal Batas : <?= date_format($date_batas, "d-m-Y H:i:s"); ?><br />
+                </div>
+            </div>
             <br /><br />
             <table id="table_id" class="table table-striped table-bordered" cellspacing="0" width="100%">
                 <thead>
                     <tr>
                         <th rowspan="2">No</th>
+                        <th rowspan="2">Waktu Pengerjaan</th>
                         <th rowspan="2">Nama</th>
                         <th colspan="3">Pre Games</th>
                         <th colspan="3">Post Games</th>
@@ -36,18 +49,33 @@
                 <tbody>
                     <?php
                     $i = 1;
-                    foreach ($tampildata as $detail) { ?>
+                    foreach ($tampildata as $detail) {
+                        $date = date_create($detail->waktu_pengerjaan);
+                    ?>
                         <tr>
                             <td><?= $i; ?></td>
+                            <td><?= date_format($date, "d-m-Y H:i:s"); ?></td>
                             <td><?= $detail->fullname; ?></td>
                             <td><?= $detail->waktu_pretest; ?></td>
                             <td><?= $detail->pre_fault_counter; ?></td>
-                            <td><?= $detail->pre_status; ?></td>
+                            <td><?php if ($detail->pre_status == 1) {
+                                    echo "<b>Berhasil</b>";
+                                } else {
+                                    echo "<b>Gagal</b>";
+                                } ?></td>
                             <td><?= $detail->post_waktu_pengerjaan; ?></td>
                             <td><?= $detail->post_fault_counter; ?></td>
-                            <td><?= $detail->post_status; ?></td>
+                            <td><?php if ($detail->post_status == 1) {
+                                    echo "<b>Berhasil</b>";
+                                } else {
+                                    echo "<b>Gagal</b>";
+                                } ?></td>
                             <td><?= $detail->expe_waktu_pengerjaan; ?></td>
-                            <td><?= $detail->expe_status; ?></td>
+                            <td><?php if ($detail->expe_status == 1) {
+                                    echo "<b>Berhasil</b>";
+                                } else {
+                                    echo "<b>Gagal</b>";
+                                } ?></td>
                         </tr>
                     <?php $i++;
                     } ?>
@@ -55,6 +83,7 @@
                 <tfoot>
                     <tr>
                         <th rowspan="2">No</th>
+                        <th rowspan="2">Waktu Pengerjaan</th>
                         <th rowspan="2">Nama</th>
                         <th>Waktu</th>
                         <th>Jml Kesalahan</th>
